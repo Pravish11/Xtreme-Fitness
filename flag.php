45a01185bb0fedc79569683ec5993537f534853e
Digumber Pravish <80615040+Pravish11@users.noreply.github.com>
2022-06-09 18:00:36 +0400

    Delete flag.php

--- flag.php
+++
@@ -1,9 +0,0 @@
-<?php
-$review_id=$_GET['review_id'];
-require_once "includes/db_connect.php";
-$Query="UPDATE review
-SET flag='1'
-WHERE review_id=$review_id";
-$conn->exec($Query);
-header("Refresh:0;url=home.php");
-?>
